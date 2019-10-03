c70b1ee662e893ece20dca6c212c5d4b56f4f8b4
sethlogan <31042154+sethloganalps@users.noreply.github.com>
2019-10-03 10:26:04 -0400

    added phone app

---
+++ reserve_gui/app/src/main/java/com/alps/alna172991/reserve_gui/SettingsActivity.java
@@ -0,0 +1,52 @@
+package com.alps.alna172991.reserve_gui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        PreferenceFragment preferenceFragment = new SettingsFragment();
+        SettingsFragment settingsFragment = (SettingsFragment) preferenceFragment;
+        settingsFragment.setContext(getApplicationContext());
+        getFragmentManager().beginTransaction().replace(android.R.id.content, preferenceFragment).commit();
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+    @Override
+    public  void onPause(){
+        super.onPause();
+
+    }
+
+    @Override
+    public  void onDestroy(){
+        super.onDestroy();
+
+    }
+}
