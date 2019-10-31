97dea564faa0c49d366163f0c5ea27651970981b
elias <equmsieh@oakland.edu>
2019-10-30 20:27:28 -0400

    elias

--- ReServeAPI_v2.0/ReServeModel.Designer.cs
+++ ReServeAPI_v2.0/ReServeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\team2capstone\ReServeAPI_v2.0\ReServeModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Eq_94\OneDrive\Documents\GitHub\team2capstone\ReServeAPI_v2.0\ReServeModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
