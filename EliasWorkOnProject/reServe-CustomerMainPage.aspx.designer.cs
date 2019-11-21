4e2a4d901ca207098b80f693616b71305499ebc7
MaryShiner <mewcat6@gmail.com>
2019-11-21 10:23:45 -0500

    11/21 Changes Table Config

--- EliasWorkOnProject/reServe-CustomerMainPage.aspx.designer.cs
+++ EliasWorkOnProject/reServe-CustomerMainPage.aspx.designer.cs
@@ -7,11 +7,18 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ReServeAPI_v2._0
-{
-
-
-    public partial class reServe_CustomerMainPage
-    {
+namespace ReServeAPI_v2._0 {
+    
+    
+    public partial class reServe_CustomerMainPage {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
